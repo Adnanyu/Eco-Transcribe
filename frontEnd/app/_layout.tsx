61a0a5ac7639b5bd25a1922bbd53e3e2814e9083
adnan <adnanyu>
2025-04-24 01:56:01 +0300

    modified layout to reflet new tabs

--- frontEnd/app/_layout.tsx
+++ frontEnd/app/_layout.tsx
@@ -1,14 +1,14 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { TouchableOpacity, View, Text } from 'react-native';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -31,8 +31,34 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="recordings/[id]" options={{title: 'Recording Detail', headerBackTitle: 'Recordings', headerTintColor: '#FA2E47'}}/>
+        <Stack.Screen name="recordings/[id]" options={({ route }) => {
+          return {
+          title: 'Recording Detail',
+          headerBackTitle: 'Recordings',
+          headerTintColor: '#FA2E47', 
+          }
+        }
+      }/>
         <Stack.Screen name="+not-found" />
+        <Stack.Screen name="recordings/edit/[id]"
+          options={{
+            title: 'Edit Recording',
+            headerBackTitle: 'Cancel',
+            headerTintColor: '#FA2E47',
+            headerRight: () => (
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <TouchableOpacity
+                  onPress={() => {}} 
+                  style={{
+                    paddingHorizontal: 15, 
+                    paddingVertical: 5, 
+                  }}
+                >
+                  <Text style={{ color: '#FA2E47', fontSize: 16 }}>Save</Text>
+                </TouchableOpacity>
+              </View>
+            )
+          }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
