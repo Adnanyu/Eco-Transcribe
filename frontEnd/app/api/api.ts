5ff68738474bbc940da3d29d763939422416a1bb
adnan <adnanyu>
2025-05-01 03:33:24 +0300

    added trnascript endpoints

--- frontEnd/app/api/api.ts
+++ frontEnd/app/api/api.ts
@@ -1,12 +1,13 @@
 import { Alert } from "react-native";
 import * as FileSystem from 'expo-file-system';
 import { Recording, Segments, Transcript } from "../types/types";
+import axios from "axios";
 
 export const getRecordings = async (): Promise<Recording[]> => {
     try {
         const response = await fetch('http://localhost:8080/api/recordings');
         if (!response.ok) {
-            throw new Error('Failed to fetch recording');
+            throw new Error('Failed to fetch recordings');
             }
         const data = await response.json();
         return data; 
@@ -34,7 +35,7 @@ export const getSegments = async (id: string): Promise<Segments[]> => {
     try {
         const response = await fetch(`http://localhost:8080/api/segments/${id}`);
         if (!response.ok) {
-            throw new Error('Failed to fetch recording');
+            throw new Error('Failed to fetch segments');
         }
         const data = await response.json();
         return data["segments"];
@@ -47,7 +48,7 @@ export const getTranscript = async (id: string): Promise<Transcript | string> =>
     try {
         const response = await fetch(`http://localhost:8080/api/transcripts/${id}`);
         if (!response.ok) {
-            throw new Error('Failed to fetch recording');
+            throw new Error('g');
         }
         const data = await response.json();
         return data; 
@@ -79,4 +80,64 @@ export const uploadFile = async (fileUri: string | null): Promise<void> => {
       console.error('Error uploading file:', error);
       Alert.alert('Error uploading file');
     }
-  };
+};
+  
+export const updateRecording = async (recording: Recording): Promise<void> => {
+    try {
+        const response = await axios.put(`http://localhost:8080/api/recordings/${recording.id}`, recording);
+        if (response.status == 204) {
+            Alert.alert('Recording updated Successfuly');
+        } else {
+            Alert.alert('updating recording failed');
+        }
+        
+    } catch (e) {
+        console.log(e)
+        throw e
+    }
+};
+
+export const updateTranscript = async (transcript: Transcript): Promise<number> => {
+    try {
+        const response = await axios.put(`http://localhost:8080/api/transcripts/${transcript.recordingId}`, transcript);
+        if (response.status == 204) {
+            Alert.alert('Transcripts updated Successfuly');
+            return response.status
+        } else {
+            Alert.alert('updating transcript failed');
+            return response.status
+        }
+    } catch (e) {
+        console.log(e)
+        throw e;
+    }
+};
+
+export const createTranscript = async (recording: Recording): Promise<void> => {
+    try {
+        const response = await axios.post(`http://localhost:8080/api/transcripts/${recording.id}`)
+        if (response.status === 201) {
+            Alert.alert(`transcript is created succesfully for ${recording.title}`)
+        } else {
+            Alert.alert('createding transcript failed');
+        }
+    } catch (e){
+        console.log(e);
+        throw e;
+    }
+}
+
+export const deleteTrancript = async (TranscriptId: string): Promise<void> => {
+    try {
+        console.log(`http://localhost:8080/api/transcripts/${TranscriptId}`)
+        const response = await axios.delete(`http://localhost:8080/api/transcripts/${TranscriptId}`)
+        if (response.status === 204) {
+            Alert.alert(`transcript is deleted succesfully for ${TranscriptId}`);
+        } else {
+            Alert.alert('deleting transcript failed');
+        }
+    } catch (e){
+        console.log(e);
+        throw e;
+    }
+}
