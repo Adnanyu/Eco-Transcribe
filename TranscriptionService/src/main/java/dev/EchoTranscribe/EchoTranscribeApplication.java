e6876aba4bd6c7f6081fbde7dcb39215f4f10c6a
adnan <adnanyu>
2025-05-10 04:33:12 +0300

    added annotation for spring jdbc and mongo repositories distinction and also Eureka server and client annotation to coonect with microservices'

--- TranscriptionService/src/main/java/dev/EchoTranscribe/EchoTranscribeApplication.java
+++ TranscriptionService/src/main/java/dev/EchoTranscribe/EchoTranscribeApplication.java
@@ -2,8 +2,23 @@ package dev.EchoTranscribe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@Configuration
+@EnableEurekaServer
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = MongoDataAutoConfiguration.class)
+@EnableMongoRepositories(basePackages = "dev.EchoTranscribe.segments")
+@EnableJdbcRepositories(basePackages = {
+    "dev.EchoTranscribe.transcripts",
+	"dev.EchoTranscribe.records",
+	"dev.EchoTranscribe.summaries"
+})
 public class EchoTranscribeApplication {
 
 	public static void main(String[] args) {
