a0a2a374110e7d093fa52dde6891affa8a7c5788
adnan <adnanyu>
2025-05-13 00:58:00 +0300

    added api calls for summary

--- frontEnd/app/api/api.ts
+++ frontEnd/app/api/api.ts
@@ -1,6 +1,6 @@
 import { Alert } from "react-native";
 import * as FileSystem from 'expo-file-system';
-import { Recording, Segments, Transcript } from "../types/types";
+import { Recording, Segments, Summary, Transcript } from "../types/types";
 import axios from "axios";
 
 export const getRecordings = async (): Promise<Recording[]> => {
@@ -141,6 +141,57 @@ export const createTranscript = async (recording: Recording): Promise<Transcript
 export const deleteTrancript = async (transcript: Transcript): Promise<number> => {
     try {
         const response = await axios.delete(`http://localhost:8080/api/transcripts/${transcript.recordingId}`)
+        return response.status
+    } catch (e){
+        console.log(e);
+        throw e;
+    }
+}
+
+export const getSummaries = async (): Promise<Summary[]> => {
+    try {
+        const response = await fetch(`http://localhost:8080/api/summaries`);
+        if (!response.ok) {
+            throw new Error('g');
+        }
+        const data = await response.json();
+        return data; 
+    } catch (e) {
+        console.log(e);
+        throw e
+    }
+}
+
+export const createSummary = async (recording: Recording): Promise<Summary> => {
+    try {
+        const response = await axios.post(`http://localhost:8080/api/summaries/${recording.id}`)
+        if (response.status !== 201) {
+            throw new Error(response.toString());
+        }
+        console.log('location is: ', response.headers.location)
+        const summaryResponse = await fetch(response.headers.location);
+        const data = await summaryResponse.json();
+        return data; 
+        // console.log(response);
+    } catch (e){
+        console.log(e);
+        throw e;
+    }
+}
+
+export const updateSummary = async (summary: Summary): Promise<number> => {
+    try {
+        const response = await axios.put(`http://localhost:8080/api/summaries/${summary.recordingId}`, summary);
+        return response.status
+    } catch (e) {
+        console.log(e)
+        throw e;
+    }
+};
+
+export const deleteSummary = async (summary: Summary): Promise<number> => {
+    try {
+        const response = await axios.delete(`http://localhost:8080/api/transcripts/${summary.recordingId}`)
         return response.status
     } catch (e){
         console.log(e);
